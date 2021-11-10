a4e5cb7cf0d6b9ae8290383a265647fde8c6ddd3
DenisOluoch <oluoch2.0.1.8denis@gmail.com>
2021-11-10 11:40:41 +0300

    New Commit

--- src/App.js
+++ src/App.js
@@ -1,120 +1,58 @@
-import "./App.css";
-import { useState } from "react";
-import Axios from "axios";
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-function App() {
-  const [name, setName] = useState("");
-  const [author, setAuthor] = useState('');
-  const [bookList, setBookList] = useState([]);
+var flash = require('express-flash');
+var session = require('express-session');
+var mysql = require('mysql');
+var connection  = require('./lib/db');
 
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var booksRouter = require('./routes/books');
 
-  const addBook = () => {
-    console.log("Add a book")
-    Axios.post("http://localhost:3306/create", {
-      name: name,
-      author: author,
-    }).then(() => {
-      setBookList([
-        ...bookList,
-        {
-          name: name,
-          author: author,
-        },
-      ]);
-    });
-  };
+var app = express();
 
-  const getBooks = () => {
-    Axios.get("http://localhost:3306/books").then((response) => {
-      setBookList(response.data);
-    });
-  };
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
 
-  // const updateBook = (id) => {
-  //   Axios.put("http://localhost:3306/update", { wage: newWage, id: id }).then(
-  //     (response) => {
-  //       setBookList(
-  //         bookList.map((val) => {
-  //           return val.id === id
-  //             ? {
-  //                 id: val.id,
-  //                 name: val.name,
-  //                 country: val.country,
-  //                 age: val.age,
-  //                 position: val.position,
-  //                 wage: newWage,
-  //               }
-  //             : val;
-  //         })
-  //       );
-  //     }
-  //   );
-  // };
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'views')));
+app.set('view engine', 'ejs');
 
-  const deleteBook = (id) => {
-    Axios.delete(`http://localhost:3306/delete/${id}`).then((response) => {
-      setBookList(
-        bookList.filter((val) => {
-          return val.id !== id;
-        })
-      );
-    });
-  };
+app.use(session({ 
+    cookie: { maxAge: 60000 },
+    store: new session.MemoryStore,
+    saveUninitialized: true,
+    resave: 'true',
+    secret: 'secret'
+}))
 
-  return (
-    <div className="App">
-      <div className="information">
-        <label>Name:</label>
-        <input
-          type="text"
-          onChange={(event) => {
-            setName(event.target.value);
-          }}
-        />
-        <label>Author:</label>
-        <input
-          type="text"
-          onChange={(event) => {
-            setAuthor(event.target.value);
-          }}
-        />
-        
-        <button onClick={addBook}>Add Book</button>
-      </div>
-      <div className="books">
-        <button onClick={getBooks}>Show Books</button>
+app.use(flash());
 
-        {bookList.map((val, key) => {
-          return (
-            <div className="book">
-              <div>
-                <h3>Name: {val.name}</h3>
-                <h3>Author: {val.author}</h3>
-              </div>
-              <div>
-                {/* <button
-                  onClick={() => {
-                    updateBook(val.id);
-                  }}
-                >
-                  {" "}
-                  Update
-                </button> */}
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/books', booksRouter);
 
-                <button
-                  onClick={() => {
-                    deleteBook(val.id);
-                  }}
-                >
-                  Delete
-                </button>
-              </div>
-            </div>
-          );
-        })}
-      </div>
-    </div>
-  );
-}
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
 
-export default App;
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
